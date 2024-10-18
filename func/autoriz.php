a83f09d335a85f230181eb2ea20039bbbc673e2a
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2024-10-18 21:44:12 +0700

    add: form rega and autorize

--- func/autoriz.php
+++ func/autoriz.php
@@ -4,51 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/normalize.css">
-    <link rel="stylesheet" href="../css/style.css" />
     <title>Панель управления</title>
-
 </head>
 
-<body>
+<body style="background-image: url(../img/kat.gif); background-size: cover;
+background-repeat: no-repeat;">
     <style>
-        h1 {
-            text-align: center;
-            font-weight: 900;
-            margin-top: 15px;
-            font-size: 40px;
+        * {
+            box-sizing: border-box;
+            font-family: monospace;
         }
 
+        h1,
+        p,
+        a {
+            margin: 0;
+            padding: 0;
+        }
 
+        .header-admin-box-title {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
 
-        .header-box {
+        .header {
+            background-color: gray;
+            padding: 20px 0;
+        }
+
+        .container {
+            max-width: 75%;
+            margin: 0 auto;
+        }
+
+        .header-admin-box {
             display: flex;
             justify-content: space-between;
             align-items: center;
         }
 
-        .header-logo {}
-
         .validation {
-            color: #595959;
-            font-size: 20px;
+            font-size: 16px;
+            font-weight: 600;
         }
 
-        .serch {
-            margin-top: 45px;
-            background-color: none;
-        }
-
-        .car-serch {
-            width: 300px;
-        }
-
-        .serch-wpap {
-            display: flex;
-            justify-content: space-between;
-            align-items: end;
+        .true-folse-content {
+            display: block;
+            width: 680px;
+            margin: 0 auto;
+            background-color: white;
+            color: black;
+            font-size: 25px;
+            padding-bottom: 25px;
+            text-align: center;
+            font-weight: 900;
+            margin-top: 250px;
+            border-radius: 10px;
         }
     </style>
+
     <?php
     require_once "./config.php";
     $email = $_POST['email'];
@@ -56,50 +71,76 @@
 
     ?>
 
-    <header class="header">
+    <header class=" header">
         <div class="container">
-            <div class="header-box">
-                <div class="container">
-                    <h1 class="header-admin-title">Панель администратора
-                        <img src="./img/logo.png" alt="radio-images" width="100px" class="header-logo">
+            <div class="header-admin-box">
+
+                <div class="header-admin-box-title">
+                    <h1 class="header-admin-title">
+                        <i class="fa-solid fa-user"></i>
+                        Панель администратора
                     </h1>
-                    <div class="validation">
-                        <?php
-                        // Подготавливаем SQL-запрос
-                        $sql = "SELECT * FROM users WHERE email = ?";
-                        $stmt = $conn->prepare($sql);
-                        $stmt->bind_param('s', $email);
-                        $stmt->execute();
+                </div>
+                <div class="validation">
+                    <?php
+                    // Подготавливаем SQL-запрос
+                    $sql = "SELECT * FROM users WHERE email = ?";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param('s', $email);
+                    $stmt->execute();
 
-                        $result = $stmt->get_result();
-                        $user = $result->fetch_assoc();
+                    $result = $stmt->get_result();
+                    $user = $result->fetch_assoc();
 
-                        // Проверяем, найден ли пользователь и верен ли пароль
-                        if ($user && password_verify($password, $user['password'])) {
-                            $_SESSION['auth'] = true;
-                            $_SESSION['username'] = $user['user_name'];
-                            $_SESSION['lastname'] = $user['last_name'];  // Сохраняем имя пользователя в сессии
-                            echo "Вы вошли как: " . $_SESSION['username'] . " " . $_SESSION['lastname'] . " | ";  // Выводим пользователя
-                        } else {
-                            echo "Авторизация не удалась <br> <a href='index.php'>Вернуться на начало</a> <br>";
-                        }
-                        ?>
-                        <a href="/" class="logaut">Выйти</a>
-                    </div>
+                    // Проверяем, найден ли пользователь и верен ли пароль
+                    if ($user && password_verify($password, $user['password'])) {
+                        $_SESSION['auth'] = true;
+                        $_SESSION['username'] = $user['user_name'];
+                        $_SESSION['lastname'] = $user['last_name'];  // Сохраняем имя пользователя в сессии
+                        echo "Вы вошли как: " . $_SESSION['username'] . " " . $_SESSION['lastname'] . " | ";  // Выводим пользователя
+                    } else {
+                        echo "Авторизация не удалась <br> <a href='index.php'>Вернуться на начало</a> <br>";
+                    }
+                    ?>
+                    <a href="/" class="logaut">Выйти</a>
                 </div>
             </div>
         </div>
+
     </header>
 
     <!-- end header -->
-    <?php
-    // проверяем авторизован
-    if (!empty($_SESSION['auth'])) {
-        require_once './content.html';
-    } else {
-        echo "<br>!Пользователь не авторизован!!!";
-    }
-    ?>
+
+
+
+    <div class="true-folse-content">
+        <?php
+        // массив месяцев
+        $arr = [
+            'январь',
+            'февраль',
+            'март',
+            'апрель',
+            'май',
+            'июнь',
+            'июль',
+            'август',
+            'сентябрь',
+            'октябрь',
+            'ноябрь',
+            'декабрь'
+        ];
+
+        // проверяем авторизован если да показываем дату
+        if (!empty($_SESSION['auth'])) {
+            require_once './content.html';
+        } else {
+            echo "<br>!Пользователь не авторизован!!!" . "<br>";
+            $month = date('n') - 1;
+            echo $arr[$month] . ' ' . date('d, Y');
+        }
+        ?>
+    </div>
 
 
     <script src="https://kit.fontawesome.com/cef99322ba.js" crossorigin="anonymous"></script>
